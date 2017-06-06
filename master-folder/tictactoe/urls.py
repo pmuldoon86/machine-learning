3d8aa8e32d6cc3250a53797e78cfad346542da4e
Paula Muldoon <pmuldoon@umich.edu>
2017-06-06 18:02:29 +0100

    template rendering

--- master-folder/tictactoe/urls.py
+++ master-folder/tictactoe/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^', include('ttt.urls')),
+    url(r'^date/', include('ttt.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- master-folder/ttt/views.py
+++ master-folder/ttt/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# from django.template import loader
 
 def index(request):
-    return HttpResponse("Welcome to tictactoe!")
+    return render(request, 'ttt/index.html')
 
 
 # Create your views here.
