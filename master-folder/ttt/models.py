7b7f5dfbaae43b2689b931b74a81d9a29e6c7d4c
SimonTanner <simonthetanner@hotmail.com>
2017-06-07 15:46:50 +0100

    Adds logic for diagonal win

--- master-folder/ttt/models.py
+++ master-folder/ttt/models.py
@@ -21,27 +21,35 @@ def check_for_win():
 # check for rows are the same
 
     if board[0][0] == board[0][1] and board[0][0] == board[0][2]:
-      print('win') 
-    elif board[1][0] == board[1][1] and board[1][0] == board[1][2]: 
+      print('win')
+    elif board[1][0] == board[1][1] and board[1][0] == board[1][2]:
       print('win')
     elif board[2][0] == board[2][1] and board[2][0] == board[2][2]:
       print('win')
-    else: 
+    else:
       print('no win')
 
 def iterate_win_check_row():
     for r in board:
-      if r[0] == r[1] == r[2]:
-        print('win') 
+      if r[0] == r[1] == r[2] == "X":
+        print('win')
       else:
         print('no win')
 
 def iterate_win_check_column():
-    for c in board:
-      if [0]c == [1]c == [2]c:
-        print('win') 
+    for c in range(0, len(board[0])):
+      if board[0][c] == board[1][c] == board[2][c] == "X":
+        print('win')
       else:
         print('no win')
+
+def win_check_diag():
+    if board[0][0] == board[1][1] == board[2][2] == "X":
+        print("win")
+    elif board[0][2] == board[1][1] == board[2][0] == "X":
+        print("win")
+    else:
+        print("no win")
 
 
 print_board(board)
