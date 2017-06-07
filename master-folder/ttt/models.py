7e0b3743d61e86d9628cbc21f26cbd944c4e05ea
Sandy Cochrane <a.x.cochrane@gmail.com>
2017-06-07 16:24:07 +0100

    added diag method, started chaining methods together

--- master-folder/ttt/models.py
+++ master-folder/ttt/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 board = []
 
-for row in range(3):
+for row in range(3): 
     board.append([])
     for column in range(3):
         board[row].append('.')
@@ -17,31 +17,43 @@ def input_marker(row, column, input_type):
     board[row][column] = input_type
     print_board(board)
 
-def check_for_win():
-# check for rows are the same
-
-    if board[0][0] == board[0][1] and board[0][0] == board[0][2]:
-      print('win') 
-    elif board[1][0] == board[1][1] and board[1][0] == board[1][2]: 
-      print('win')
-    elif board[2][0] == board[2][1] and board[2][0] == board[2][2]:
-      print('win')
-    else: 
-      print('no win')
-
 def iterate_win_check_row():
     for r in board:
       if r[0] == r[1] == r[2]:
-        print('win') 
-      else:
-        print('no win')
+        return('win') 
 
 def iterate_win_check_column():
-    for c in board:
-      if [0]c == [1]c == [2]c:
-        print('win') 
+    for c in range(0, len(board[0])):
+      column = 0
+      if board[0][c] == board[1][c] == board[2][c]:
+        return('win') 
+
+def win_check_diag():
+      if board[0][0] == board[1][1] == board[2][2]:
+        return('win')   
+      elif board[0][2] == board[1][1] == board[2][0]:
+        return('win') 
       else:
-        print('no win')
+        return('no win')
+
+def check_win()
+    iterate_win_check_row
+    iterate_win_check_column
+    win_check_diag
+
+
+
 
 
 print_board(board)
+
+
+
+# board
+# [
+# [r1[0][1][2]]
+# [r2[0][1][2]]
+# [r3[0][1][2]]
+# ]
+
+
