c1f9b7519e1f037d29fedf7d0cdc1901b52c351b
Sandy Cochrane <a.x.cochrane@gmail.com>
2017-06-07 13:20:50 +0100

    create function to check for win

--- master-folder/ttt/models.py
+++ master-folder/ttt/models.py
@@ -7,11 +7,41 @@ board = []
 for row in range(3):
     board.append([])
     for column in range(3):
-        board[row].append('x')
-
+        board[row].append('.')
 
 def print_board(board):
     for row in board:
         print(" ".join(row))
+
+def input_marker(row, column, input_type):
+    board[row][column] = input_type
+    print_board(board)
+
+def check_for_win():
+# check for rows are the same
+
+    if board[0][0] == board[0][1] and board[0][0] == board[0][2]:
+      print('win') 
+    elif board[1][0] == board[1][1] and board[1][0] == board[1][2]: 
+      print('win')
+    elif board[2][0] == board[2][1] and board[2][0] == board[2][2]:
+      print('win')
+    else: 
+      print('no win')
+
+def iterate_win_check_row():
+    for r in board:
+      if r[0] == r[1] == r[2]:
+        print('win') 
+      else:
+        print('no win')
+
+def iterate_win_check_column():
+    for c in board:
+      if [0]c == [1]c == [2]c:
+        print('win') 
+      else:
+        print('no win')
+
 
 print_board(board)
