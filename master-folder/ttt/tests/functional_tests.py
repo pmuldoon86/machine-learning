fa8d4f0459803eea54a93a87b5020e65f7c37482
SimonTanner <simonthetanner@hotmail.com>
2017-06-08 14:55:50 +0100

    Refactoring feature test for game in browser

--- master-folder/ttt/tests/functional_tests.py
+++ master-folder/ttt/tests/functional_tests.py
@@ -10,12 +10,10 @@ class NewGametest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def test_game(self):
+        self.browser.get('http://localhost:8000')
 
-
-
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-
-assert 'TTT' in browser.title, "Browser title was " + browser.title
+        self.assertIn('TTT', self.browser.title)
+        self.fail('Finish the test!')
 
 browser.quit()
