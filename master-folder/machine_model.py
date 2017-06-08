2c9273d37af3cacf7c2d170b68ea79d97a10c011
SimonTanner <simonthetanner@hotmail.com>
2017-06-08 19:22:10 +0100

    1st attempt at building game logic array

--- master-folder/machine_model.py
+++ master-folder/machine_model.py
@@ -2,6 +2,8 @@ import copy
 
 memory = []
 
+memory2 = [[[1],[[2],[[3]]],[[3],[[2]]]],[[2],[[1],[[3]]],[[3],[[1]]]],[[3],[[2],[[1]]],[[1],[[2]]]]]
+
 states = [[[1, 1]], [[2,1]], [[3, 1]], [[4, 1]], [[5, 1]], [[6, 1]], [[7, 1]], [[8, 1]], [[9, 1]]]
 
 simple_states = [[[1, 1]], [[2,1]], [[3, 1]]]
@@ -12,23 +14,35 @@ def append_states_to_mem(memory2, states2):
     loop_states(memory2, s1)
     return memory2
 
-
+v = 1
 def loop_states(memory1, states1):
+    global v
     for i in range(0, len(states1)):
         s2 = []
-        if states1 > 0:
-            if i == 0:
-                memory1[i].append(states1[i + 1: len(states1)])
-            elif i == len(states1) - 1:
-                memory1[i].append(states1[0:len(states1)-1])
-            else:
-                for n in range(0, len(states1)):
-                    if i != n:
-                        s2.append(states1[n])
-                memory1[i].append(s2)
 
+        if i == 0:
+            s2 = states1[i + 1: len(states1)]
+        elif i == len(states1) - 1:
+            s2 = states1[0:len(states1)]
+        else:
+            for n in range(0, len(states1)):
+                if i != n:
+                    s2.append(states1[n])
+        if len(s2) > 0:
+            memory1[i].append(s2)
+        print(memory1[i])
+        print(i)
+        print("v = " + str(v))
+        v +=1
+        try:
             loop_states(memory1[i][1], memory1[i][1])
+        except IndexError:
+            break
+        else:
+            break
 
 memory = append_states_to_mem(memory, simple_states)
 
-print memory
+print(memory)
+
+[[[1, 1], [[[2, 1], [[[3, 1]]]], [[3, 1]]]], [[2, 1]], [[3, 1]]]
