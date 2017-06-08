69d2f75a600e93597b1948d6174d863f11b8c4b6
SimonTanner <simonthetanner@hotmail.com>
2017-06-08 23:56:57 +0100

    2nd decision branch generated

--- master-folder/machine_model.py
+++ master-folder/machine_model.py
@@ -4,42 +4,51 @@ memory = []
 
 memory2 = [[[1],[[2],[[3]]],[[3],[[2]]]],[[2],[[1],[[3]]],[[3],[[1]]]],[[3],[[2],[[1]]],[[1],[[2]]]]]
 
-states = [[[1, 1]], [[2,1]], [[3, 1]], [[4, 1]], [[5, 1]], [[6, 1]], [[7, 1]], [[8, 1]], [[9, 1]]]
+states = [[1, 1], [2,1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [8, 1], [9, 1]]
 
-simple_states = [[[1, 1]], [[2,1]], [[3, 1]]]
+simple_states = [[1, 1], [2,1], [3, 1]]
 
 def append_states_to_mem(memory2, states2):
-    memory2 = copy.deepcopy(states2)
-    s1 = copy.deepcopy(states2)
-    loop_states(memory2, s1)
+    memory2.append(states2)
+    #s1 = copy.deepcopy(states2)
+    loop_states(memory2, memory2)
     return memory2
 
 v = 1
-def loop_states(memory1, states1):
+
+def loop_states(memory1, memory2):
     global v
+    #memory1.append[()]
+    states1 = memory1[-1]
+    s1 = []
     for i in range(0, len(states1)):
         s2 = []
 
         if i == 0:
             s2 = states1[i + 1: len(states1)]
         elif i == len(states1) - 1:
-            s2 = states1[0:len(states1)]
+            s2 = states1[0:len(states1)-1]
         else:
             for n in range(0, len(states1)):
                 if i != n:
                     s2.append(states1[n])
         if len(s2) > 0:
-            memory1[i].append(s2)
-        print(memory1[i])
+            s1.append(s2)
+
+        #print(memory1[i])
         print(i)
         print("v = " + str(v))
         v +=1
-        try:
-            loop_states(memory1[i][1], memory1[i][1])
-        except IndexError:
-            break
-        else:
-            break
+    memory2.append(s1)
+    print(memory2)
+    # for l in range(0, len(memory[-1])):
+    #     loop_states(memory1[-1][l], memory2)
+    # try:
+    #     loop_states(memory1[-1])
+    # except IndexError:
+    #     break
+    # else:
+    #     break
 
 memory = append_states_to_mem(memory, simple_states)
 
