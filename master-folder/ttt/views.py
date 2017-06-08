8287cc50d8149e8ff58ee264ef2dfd16c7a53cf7
Paula Muldoon <pmuldoon@umich.edu>
2017-06-08 15:34:01 +0100

    failing to resolve to home page view

--- master-folder/ttt/views.py
+++ master-folder/ttt/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 # from django.template import loader
 
-def index(request):
-    return render(request, 'ttt/index.html')
+# def index(request):
+#     return render(request, 'ttt/index.html')
 
 
 # Create your views here.
+
+home_page = None
