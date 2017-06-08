87bc2244569eb496a2c0d03f1f53f573b4c1b18e
Paula Muldoon <pmuldoon@umich.edu>
2017-06-08 12:21:28 +0100

    useful error message in test

--- master-folder/ttt/tests/functional_tests.py
+++ master-folder/ttt/tests/functional_tests.py
@@ -3,4 +3,6 @@ from selenium import webdriver
 browser = webdriver.Firefox()
 browser.get('http://localhost:8000')
 
-assert 'TTT' in browser.title
+assert 'TTT' in browser.title, "Browser title was " + browser.title
+
+browser.quit()
