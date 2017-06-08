03f03cca13f95d32e66fd5e2aa0ba76825a9b9df
SimonTanner <simonthetanner@hotmail.com>
2017-06-08 16:11:24 +0100

    Test for homepage now passes

--- master-folder/ttt/views.py
+++ master-folder/ttt/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+from django.http import HttpResponse
 # from django.template import loader
 
 # def index(request):
@@ -8,5 +8,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home_page():
+def home_page(request):
+    return HttpResponse('<html><title> TTT </title></html>')
     pass
